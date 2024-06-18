636e74a0db6d5e2b5055009e0c440be3d62da6b1
bunnycou <noahjcou@gmail.com>
2024-06-18 00:06:58 -0400

    removed redirect stuff because I cannot have it

--- redirect.js
+++
@@ -1,9 +0,0 @@
-subdomain = window.location.host.split(".")[0]
-
-if (subdomain == "map") {
-    window.location.href = "./map/"
-}
-
-if (subdomain == "mwi") {
-    window.location.href = "./mwi/"
-}
